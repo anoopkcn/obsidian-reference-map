55385a377acc9c9dd83f407840ab317f1d6b839e
Anoop Chandran <anoopkcn@gmail.com>
2023-01-04 01:11:19 +0100

    Check for values and show if present + only show year instead of date

--- src/components/PaperTitleGroup.tsx
+++ src/components/PaperTitleGroup.tsx
@@ -4,12 +4,21 @@ import { SemanticPaper } from "src/types";
 
 export const PaperTitleGroup = (props: { paper: SemanticPaper }) => {
 	const paper: SemanticPaper = props.paper;
+	const paperTitle = paper.title ? paper.title : "Unknown Title";
+	const firstAuthor = paper.authors[0]
+		? paper.authors[0].name
+		: "Unknown Author";
+	const year = paper.year ? paper.year : "Unknown Year";
+	let authorID = null;
+	if (paper.authors[0]) {
+		authorID = paper.authors[0].authorId;
+	}
 	return (
 		<>
 			<div className="orm-paper-title-group">
 				<span className="orm-paper-title">
 					<a href={`${SEMANTICSCHOLAR_URL}/paper/${paper.paperId}`}>
-						{" " + paper.title + " "}
+						{" " + paperTitle + " "}
 					</a>
 				</span>
 				{/* <span className="orm-external-link">
@@ -18,13 +27,8 @@ export const PaperTitleGroup = (props: { paper: SemanticPaper }) => {
 			</div>
 			<div className="orm-paper-authors-group">
 				<span className="orm-paper-authors">
-					<a
-						href={`${SEMANTICSCHOLAR_URL}/author/${paper.authors[0].authorId}`}
-					>
-						{paper.authors[0].name +
-							", " +
-							paper.publicationDate +
-							" "}
+					<a href={`${SEMANTICSCHOLAR_URL}/author/${authorID}`}>
+						{firstAuthor + ", " + year}
 					</a>
 				</span>
 			</div>
