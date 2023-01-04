7c111e3ee632a5c9b5150d828aed85c0622f365c
Anoop Chandran <anoopkcn@gmail.com>
2023-01-04 02:33:35 +0100

    Button Group improvement, made it general

--- src/components/PaperButtonGroup.tsx
+++ src/components/PaperButtonGroup.tsx
@@ -5,8 +5,21 @@ import { SiOpenaccess } from "react-icons/si";
 import { SemanticPaper } from "src/types";
 import { copyElToClipboard } from "src/utils";
 
-export const PaperButtonGroup = (props: { paper: SemanticPaper }) => {
-	const paper: SemanticPaper = props.paper;
+type Props = {
+	paper: SemanticPaper;
+	setShowReferences?: React.Dispatch<React.SetStateAction<boolean>>;
+	showReferences?: boolean;
+	setShowCitations?: React.Dispatch<React.SetStateAction<boolean>>;
+	showCitations?: boolean;
+};
+
+export const PaperButtonGroup = ({
+	paper,
+	setShowReferences = undefined,
+	showReferences = false,
+	setShowCitations = undefined,
+	showCitations = false,
+}: Props) => {
 	const paperTitle = paper.title ? paper.title : "Unknown Title";
 	const firstAuthor = paper.authors[0]?.name
 		? paper.authors[0].name
@@ -21,6 +34,46 @@ export const PaperButtonGroup = (props: { paper: SemanticPaper }) => {
 		openAccessPdfUrl = paper.openAccessPdf.url
 			? paper.openAccessPdf.url
 			: "";
+	}
+
+	let citingCited = null;
+	if (setShowReferences !== undefined && setShowCitations !== undefined) {
+		const handleShowReferencesClick = () => {
+			setShowReferences(!showReferences);
+			setShowCitations(false);
+		};
+
+		const handleShowCitationsClick = () => {
+			setShowCitations(!showCitations);
+			setShowReferences(false);
+		};
+		citingCited = (
+			<>
+				<div
+					className="orm-references"
+					onClick={() => handleShowReferencesClick()}
+				>
+					{paper.referenceCount.toString()}
+				</div>
+				<div
+					className="orm-citations"
+					onClick={() => handleShowCitationsClick()}
+				>
+					{paper.citationCount.toString()}
+				</div>
+			</>
+		);
+	} else {
+		citingCited = (
+			<>
+				<div className="orm-references-2">
+					{paper.referenceCount.toString()}
+				</div>
+				<div className="orm-citations-2">
+					{paper.citationCount.toString()}
+				</div>
+			</>
+		);
 	}
 	return (
 		<div className="orm-paper-buttons">
@@ -51,12 +104,7 @@ export const PaperButtonGroup = (props: { paper: SemanticPaper }) => {
 					<FiSlash size={16} />
 				)}
 			</div>
-			<div className="orm-references-2">
-				{paper.referenceCount.toString()}
-			</div>
-			<div className="orm-citations-2">
-				{paper.citationCount.toString()}
-			</div>
+			{citingCited}
 			{/* <div className="orm-influential-citations">
 				{paper.influentialCitationCount.toString()}
 			</div> */}
--- src/components/RootReference.tsx
+++ src/components/RootReference.tsx
@@ -1,12 +1,10 @@
 import { SemanticPaper } from "src/types";
 import React, { useState } from "react";
-import { copyElToClipboard, removeNullReferences } from "src/utils";
-import { IoMdClipboard } from "react-icons/io";
-import { SiOpenaccess } from "react-icons/si";
-import { FiPaperclip, FiSlash } from "react-icons/fi";
+import { removeNullReferences } from "src/utils";
 import { ReferencesList } from "./ReferencesList";
 import { PaperTitleGroup } from "./PaperTitleGroup";
 import { CitationsList } from "./CitationsList";
+import { PaperButtonGroup } from "./PaperButtonGroup";
 
 export const RootReference = (props: {
 	key: string;
@@ -17,87 +15,21 @@ export const RootReference = (props: {
 	const [showReferences, setShowReferences] = useState(false);
 	const [showCitations, setShowCitations] = useState(false);
 
-	const handleShowReferencesClick = () => {
-		setShowReferences(!showReferences);
-		setShowCitations(false);
-	};
-
-	const handleShowCitationsClick = () => {
-		setShowCitations(!showCitations);
-		setShowReferences(false);
-	};
-
 	const references = removeNullReferences(props.references);
 	const citations = removeNullReferences(props.citations);
 
 	const rootPaper: SemanticPaper = props.rootPaper;
 
-	const paperTitle = rootPaper.title ? rootPaper.title : "Unknown Title";
-	const firstAuthor = rootPaper.authors[0]?.name
-		? rootPaper.authors[0].name
-		: "Unknown Author";
-	const year = rootPaper.year ? rootPaper.year : "Unknown Year";
-	const abstract = rootPaper.abstract
-		? rootPaper.abstract
-		: "No abstract available";
-	const bibTex = rootPaper.citationStyles.bibtex
-		? rootPaper.citationStyles.bibtex
-		: "No BibTex available";
-	let openAccessPdfUrl = "";
-	if (rootPaper.isOpenAccess) {
-		openAccessPdfUrl = rootPaper.openAccessPdf.url
-			? rootPaper.openAccessPdf.url
-			: "";
-	}
 	return (
 		<div className="orm-root-paper">
 			<PaperTitleGroup paper={rootPaper} />
-			<div className="orm-paper-buttons">
-				<div
-					className="orm-copy-bibtex"
-					onClick={() => {
-						copyElToClipboard(bibTex);
-					}}
-				>
-					<IoMdClipboard size={17} />
-				</div>
-
-				<div
-					className="orm-copy-metadata"
-					onClick={() => {
-						copyElToClipboard(
-							`${paperTitle}, ${firstAuthor}, ${year}\n${abstract}`
-						);
-					}}
-				>
-					<FiPaperclip size={16} />
-				</div>
-
-				<div className="orm-openaccess">
-					{rootPaper.isOpenAccess ? (
-						<a href={`${openAccessPdfUrl}`}>
-							<SiOpenaccess size={16} />
-						</a>
-					) : (
-						<FiSlash size={16} />
-					)}
-				</div>
-				<div
-					className={`orm-references`}
-					onClick={() => handleShowReferencesClick()}
-				>
-					{rootPaper.referenceCount.toString()}
-				</div>
-				<div
-					className="orm-citations"
-					onClick={() => handleShowCitationsClick()}
-				>
-					{rootPaper.citationCount.toString()}
-				</div>
-				{/* <div className="orm-influential-citations">
-					{rootPaper.influentialCitationCount.toString()}
-				</div> */}
-			</div>
+			<PaperButtonGroup
+				paper={rootPaper}
+				setShowReferences={setShowReferences}
+				showReferences={showReferences}
+				setShowCitations={setShowCitations}
+				showCitations={showCitations}
+			/>
 			{showReferences && <ReferencesList references={references} />}
 			{showCitations && <CitationsList citations={citations} />}
 		</div>
