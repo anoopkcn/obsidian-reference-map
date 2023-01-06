265c3523b0834040237869c6210c580cba5f507c
Anoop Chandran <anoopkcn@gmail.com>
2023-01-06 08:44:26 +0100

    File name change

--- src/viewManager.ts
+++ src/viewManager.ts
@@ -3,7 +3,7 @@ import LRUCache from 'lru-cache';
 import { SemanticPaper } from './types';
 import ReferenceMap from './main';
 import { areSetsEqual, getPaperIds } from './utils';
-import { getPaperMetadata, postPaperMetadata } from './referencemap';
+import { getPaperMetadata, postPaperMetadata } from './referenceMap';
 
 export interface DocCache {
     paperIds: Set<string>;
