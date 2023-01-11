220796548c203f1c13d09a96b1388313cffa9730
Anoop Chandran <anoopkcn@gmail.com>
2023-01-11 10:15:49 +0100

    Don't console log

--- src/viewManager.ts
+++ src/viewManager.ts
@@ -53,7 +53,6 @@ export class ViewManager {
         const cachedDoc = this.cache.has(file) ? this.cache.get(file) : null;
         // If the cached document doesn't exist or the paperIds are different, fetch the new document
         if (!cachedDoc || !areSetsEqual(cachedDoc.paperIds, paperIds)) {
-            console.log(paperIds)
             try {
                 // const paper = await getPaperMetadata(paperIds.values().next().value);
                 const rootPapers = await postPaperMetadata(paperIds);
@@ -77,7 +76,6 @@ export class ViewManager {
         const cachedDoc = this.pandocCache.has(file) ? this.pandocCache.get(file) : null;
         // If the cached document doesn't exist or the paperIds are different, fetch the new document
         if (!cachedDoc || !areSetsEqual(cachedDoc.paperIds, paperIds)) {
-            console.log(paperIds)
             try {
                 const rootPapers = await postPaperMetadata(paperIds);
                 this.pandocCache.set(file, { paperIds, rootPapers });
