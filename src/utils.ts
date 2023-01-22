b0b3caea86c84ed5ae77f9fd63cf2cd3eb931e4b
Anoop Chandran <anoopkcn@gmail.com>
2023-01-22 20:20:58 +0100

    Fix(parser): citekey end patter character is forced to be space for end of the sentence structures

--- src/utils.ts
+++ src/utils.ts
@@ -103,7 +103,7 @@ export const getCiteKeys = (content: string): Set<string> => {
     // const citekeyRegex = /@[^{]+{([^,]+),/g;
     const citekeyRegex = /@([^\s]+)/gi;
     // explain the regex 
-    const matches = content.matchAll(citekeyRegex);
+    const matches = content.replace(/[\])*`]+/, ' ').matchAll(citekeyRegex);
     if (matches) {
         for (const match of matches) {
             output.add(match[1].replace(/\)+$|\]+$|\*+$|`+$/, ''));
