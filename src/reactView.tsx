f249d2ba57e7947d89e5fdd1f8ea38b7db9557ea
Anoop K. Chandran <a.chandran@fz-juelich.de>
2023-10-15 12:06:57 +0200

    Refactor getting data for the view

--- src/reactView.tsx
+++ src/reactView.tsx
@@ -4,10 +4,7 @@ import { t } from './lang/helpers'
 import React from 'react'
 import { Root, createRoot } from 'react-dom/client'
 import { ReferenceMapList } from './components/ReferenceMapList'
-import { extractKeywords, getCiteKeyIds, getCiteKeys, getPaperIds, } from './utils'
-import { EXCLUDE_FILE_NAMES } from './constants'
-import { CiteKey, RELOAD, Reload } from './types'
-import _ from 'lodash'
+import { RELOAD, Reload } from './types'
 import { ReferenceMapData } from './referenceData'
 
 export const REFERENCE_MAP_VIEW_TYPE = 'reference-map-view'
@@ -17,33 +14,21 @@ export class ReferenceMapView extends ItemView {
 	activeMarkdownLeaf: MarkdownView
 	referenceMapData: ReferenceMapData
 	rootEl: Root
-	paperIDs: Set<string>
-	citeKeyMap: CiteKey[]
-	frontMatterString: string
-	fileNameString: string
-	basename: string
+	isUpdated: boolean
 
 	constructor(leaf: WorkspaceLeaf, plugin: ReferenceMap) {
 		super(leaf)
 		this.plugin = plugin
 		this.referenceMapData = new ReferenceMapData(plugin)
 		this.rootEl = createRoot(this.containerEl.children[1])
-		this.paperIDs = new Set()
-		this.citeKeyMap = []
-		this.frontMatterString = ''
-		this.fileNameString = ''
-		this.basename = ''
+		this.isUpdated = false
 
 		this.registerEvent(
 			this.app.metadataCache.on('changed', (file) => {
 				const activeView =
 					this.app.workspace.getActiveViewOfType(MarkdownView)
 				if (activeView && file === activeView.file) {
-					this.prepareIDs().then((isUpdated) => {
-						if (isUpdated) {
-							this.processReferences()
-						}
-					})
+					this.processReferences()
 				}
 			})
 		)
@@ -53,9 +38,7 @@ export class ReferenceMapView extends ItemView {
 				if (leaf) {
 					this.app.workspace.iterateRootLeaves((rootLeaf) => {
 						if (rootLeaf === leaf) {
-							this.prepareIDs().then(() =>
-								this.processReferences()
-							)
+							this.processReferences()
 						}
 					})
 				}
@@ -70,7 +53,7 @@ export class ReferenceMapView extends ItemView {
 			this.idSelectionHandle()
 		})
 
-		this.prepareIDs().then(() => this.processReferences())
+		this.processReferences()
 	}
 
 	getViewType() {
@@ -87,7 +70,7 @@ export class ReferenceMapView extends ItemView {
 
 	async onOpen() {
 		await this.referenceMapData.loadLibrary()
-		this.prepareIDs().then(() => this.processReferences())
+		this.processReferences()
 	}
 
 	async onClose() {
@@ -101,12 +84,12 @@ export class ReferenceMapView extends ItemView {
 			this.referenceMapData.viewManager.clearCache()
 			this.referenceMapData.resetLibraryTime()
 			await this.referenceMapData.loadLibrary()
-			this.prepareIDs().then(() => this.processReferences())
+			this.processReferences()
 		} else if (reloadType === RELOAD.SOFT) {
 			await this.referenceMapData.loadLibrary()
-			this.prepareIDs().then(() => this.processReferences())
+			this.processReferences()
 		} else if (reloadType === RELOAD.VIEW) {
-			this.prepareIDs().then(() => this.processReferences())
+			this.processReferences()
 		}
 	}
 
@@ -120,104 +103,41 @@ export class ReferenceMapView extends ItemView {
 				? editor.getSelection().trim()
 				: window.getSelection()?.toString().trim()
 
-		const isInIDs = Array.from(this.paperIDs).map((id: string) => {
+		const isInIDs = Array.from(this.referenceMapData.paperIDs).map((id: string) => {
 			id = id.replace('https://doi.org/', '');
 			return selection?.includes(id)
 		})
 		let isInCiteKeys = false
-		for (const key in this.citeKeyMap) {
-			const value = String(this.citeKeyMap[key])
+		for (const key in this.referenceMapData.citeKeyMap) {
+			const value = String(this.referenceMapData.citeKeyMap[key])
 			if (selection?.includes(key) || selection?.includes(value)) {
 				isInCiteKeys = true
 				break
 			}
 		}
 		if (isInIDs || isInCiteKeys) {
-			this.prepareIDs().then(() => this.processReferences(selection ?? ''))
+			this.processReferences(selection ?? '')
 		}
 	}, 300, true)
 
-	prepareIDs = async () => {
-		const isLibrary =
-			this.plugin.settings.searchCiteKey &&
-			this.referenceMapData.library.libraryData !== null
-		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView)
-		let fileNameString = ''
-		let frontMatterString = ''
-		let paperIDs: Set<string> = new Set()
-		let citeKeyMap: CiteKey[] = []
-		let isUpdated = false
-		this.basename = ''
-		if (activeView) {
-			if (isLibrary) this.referenceMapData.loadLibrary()
-			this.basename = activeView.file?.basename ?? ''
-			const fileContent = activeView.file ? await this.app.vault.cachedRead(activeView.file) : ''
-			paperIDs = getPaperIds(fileContent)
-
-			if (isLibrary && activeView.file) {
-				const citeKeys = getCiteKeys(
-					fileContent,
-					this.plugin.settings.findCiteKeyFromLinksWithoutPrefix,
-					this.plugin.settings.citeKeyFilter
-				)
-				citeKeyMap = getCiteKeyIds(citeKeys, this.referenceMapData.library)
-			}
-
-			if (this.plugin.settings.searchFrontMatter) {
-				if (activeView.file) {
-					const fileCache = this.app.metadataCache.getFileCache(activeView.file);
-					if (fileCache?.frontmatter) {
-						const keywords =
-							fileCache?.frontmatter?.[
-							this.plugin.settings.searchFrontMatterKey
-							];
-						if (keywords)
-							frontMatterString = extractKeywords(keywords)
-								.unique()
-								.join("+");
-					}
-				}
-			}
-			if (
-				this.plugin.settings.searchTitle &&
-				!EXCLUDE_FILE_NAMES.some(
-					(name) => this.basename.toLowerCase() === name.toLowerCase()
-				)
-			) {
-				fileNameString = extractKeywords(this.basename)
-					.unique()
-					.join('+')
-			}
-			const isPaperIDsUpdated = _.isEqual(paperIDs, this.paperIDs)
-			const isCiteKeyMapUpdated = _.isEqual(citeKeyMap, this.citeKeyMap)
-			const isFrontMatterUpdated =
-				frontMatterString === this.frontMatterString
-			const isFileNameUpdated = fileNameString === this.fileNameString
-
-			isUpdated =
-				!isPaperIDsUpdated ||
-				!isCiteKeyMapUpdated ||
-				!isFrontMatterUpdated ||
-				!isFileNameUpdated
-
-			// If there are updated changes then update the class variables
-			if (isUpdated) {
-				this.paperIDs = paperIDs
-				this.citeKeyMap = citeKeyMap
-				this.frontMatterString = frontMatterString
-				this.fileNameString = fileNameString
-			}
-		}
-		return isUpdated
-	}
-
 	processReferences = async (selection = '') => {
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView)
+		if (activeView?.file) {
+			const fileMetadataCache = activeView.file ? await this.app.vault.cachedRead(activeView.file) : ''
+			const fileCache = this.app.metadataCache.getFileCache(activeView.file);
+			this.referenceMapData.updatePaperIDs(activeView, fileMetadataCache, fileCache)
+		} else {
+			// This is needed to trigger rendering if active view is not a markdown file
+			// the prop name basename will re render the view
+			this.referenceMapData.basename = ''
+		}
+		// if (!this.isUpdated) return
 		const indexCards = await this.referenceMapData.getIndexCards(
-			this.paperIDs,
-			this.citeKeyMap,
-			this.fileNameString,
-			this.frontMatterString,
-			this.basename,
+			this.referenceMapData.paperIDs,
+			this.referenceMapData.citeKeyMap,
+			this.referenceMapData.fileNameString,
+			this.referenceMapData.frontMatterString,
+			this.referenceMapData.basename,
 			true
 		)
 		this.rootEl.render(
@@ -225,9 +145,9 @@ export class ReferenceMapView extends ItemView {
 				settings={this.plugin.settings}
 				viewManager={this.referenceMapData.viewManager}
 				library={this.referenceMapData.library}
-				basename={this.basename}
-				paperIDs={this.paperIDs}
-				citeKeyMap={this.citeKeyMap}
+				basename={this.referenceMapData.basename}
+				paperIDs={this.referenceMapData.paperIDs}
+				citeKeyMap={this.referenceMapData.citeKeyMap}
 				indexCards={indexCards}
 				selection={selection}
 			/>
--- src/referenceData.ts
+++ src/referenceData.ts
@@ -1,20 +1,31 @@
 import * as fs from 'fs'
 import * as BibTeXParser from '@retorquere/bibtex-parser'
-import { indexSort, removeNullReferences, resolvePath, setCiteKeyId } from './utils'
+import { extractKeywords, getCiteKeyIds, getCiteKeys, getPaperIds, indexSort, removeNullReferences, resolvePath, setCiteKeyId } from './utils'
 import { DEFAULT_LIBRARY, EXCLUDE_FILE_NAMES } from './constants';
 import ReferenceMap from './main';
 import { CiteKey, IndexPaper, Library } from './types';
 import _ from 'lodash';
 import { ViewManager } from './viewManager';
+import { CachedMetadata, MarkdownView } from 'obsidian';
 
 export class ReferenceMapData {
     plugin: ReferenceMap
     library: Library
     viewManager: ViewManager
+    paperIDs: Set<string>
+    citeKeyMap: CiteKey[]
+    frontMatterString: string
+    fileNameString: string
+    basename: string
     constructor(plugin: ReferenceMap) {
         this.plugin = plugin
         this.library = DEFAULT_LIBRARY
         this.viewManager = new ViewManager(plugin)
+        this.paperIDs = new Set()
+        this.citeKeyMap = []
+        this.frontMatterString = ''
+        this.fileNameString = ''
+        this.basename = ''
     }
 
     resetLibraryTime = () => {
@@ -146,6 +157,75 @@ export class ReferenceMapData {
             this.plugin.settings.sortOrderIndex
         )
     }
+
+
+    updatePaperIDs = async (activeView: MarkdownView, fileMetadataCache = '', fileCache: CachedMetadata | null = null) => {
+        const isLibrary =
+            this.plugin.settings.searchCiteKey &&
+            this.library.libraryData !== null
+        this.basename = ''
+        if (activeView) {
+            if (isLibrary) this.loadLibrary()
+            this.basename = activeView.file?.basename ?? ''
+            if (fileMetadataCache) {
+                this.paperIDs = getPaperIds(fileMetadataCache)
+            }
+            if (isLibrary && activeView.file) {
+                const citeKeys = getCiteKeys(
+                    fileMetadataCache,
+                    this.plugin.settings.findCiteKeyFromLinksWithoutPrefix,
+                    this.plugin.settings.citeKeyFilter
+                )
+                this.citeKeyMap = getCiteKeyIds(citeKeys, this.library)
+            }
+
+            if (this.plugin.settings.searchFrontMatter) {
+                if (activeView.file && fileCache) {
+                    if (fileCache?.frontmatter) {
+                        const keywords =
+                            fileCache?.frontmatter?.[
+                            this.plugin.settings.searchFrontMatterKey
+                            ];
+                        if (keywords)
+                            this.frontMatterString = extractKeywords(keywords)
+                                .unique()
+                                .join("+");
+                    }
+                }
+            }
+            if (
+                this.plugin.settings.searchTitle &&
+                !EXCLUDE_FILE_NAMES.some(
+                    (name) => this.basename.toLowerCase() === name.toLowerCase()
+                )
+            ) {
+                this.fileNameString = extractKeywords(this.basename)
+                    .unique()
+                    .join('+')
+            }
+            // const isPaperIDsUpdated = _.isEqual(paperIDs, this.paperIDs)
+            // const isCiteKeyMapUpdated = _.isEqual(citeKeyMap, this.citeKeyMap)
+            // const isFrontMatterUpdated =
+            //     frontMatterString === this.frontMatterString
+            // const isFileNameUpdated = fileNameString === this.fileNameString
+
+            // isUpdated =
+            //     !isPaperIDsUpdated ||
+            //     !isCiteKeyMapUpdated ||
+            //     !isFrontMatterUpdated ||
+            //     !isFileNameUpdated
+
+            // // If there are updated changes then update the class variables
+            // if (isUpdated) {
+            //     this.paperIDs = paperIDs
+            //     this.citeKeyMap = citeKeyMap
+            //     this.frontMatterString = frontMatterString
+            //     this.fileNameString = fileNameString
+            // }
+        }
+        return
+    }
+
 
 
 }
